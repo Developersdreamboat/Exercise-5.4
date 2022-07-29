96c397cc71f9c641559f9ffdeed178d8508f3d25
Evhenii Pavlenko <43095662+Developersdreamboat@users.noreply.github.com>
2022-07-29 11:09:47 +0300

    Comments fix

--- program.c
+++ program.c
@@ -20,15 +20,15 @@ int strend(char *s, char *t)
 {
   int counter;
   
-  /* count number of characters and place pointer to last letter of second word */
+  /* count number of characters and place pointer to last letter of second string */
   for(counter = 0; *t != '\0'; ++counter, *t++)  
     ;
   
-  /* place pointer to position of last letter of first word */
+  /* place pointer to position of last letter of first string */
   for ( ; *s != '\0'; *s++) 
     ;
     
-  /* compare letters of words from the end */
+  /* compare letters of strings from the end */
   for ( ; counter > 0 && *s == *t; counter--, *--s, *--t)
     ;
     
